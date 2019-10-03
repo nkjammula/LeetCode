89b6d86c2ecb364de1030eca9f2b593526746145
Naveen Jammula <noblenaveen@gmail.com>
2019-10-03 13:42:24 -0500

    Leet code amazon problems

--- LeetCode/ArrayStrings.cs
+++ LeetCode/ArrayStrings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Text;
 
 namespace LeetCode
@@ -84,16 +85,106 @@ namespace LeetCode
             return result;
         }
 
-       
+        public static int[] PlusOne(int[] digits)
+        {
+            int[] result = new int[digits.Length + 1];
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i] += 1;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+            result[0] = 1;
+            return result;
+        }
 
+
+        public static bool solution(int[] A, int K)
+        {  //1 1 2 3 3
+            int n = A.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (A[i] + 1 < A[i + 1])
+                    return false;
+            }
+
+            if ((A[0] != 1 && A[n - 1] != K) || ((K > A[n - 1]) || K < A[0]))
+                return false;
+
+           
+            else
+                return true;
+        }
 
 
         public static void Main()
         {
-           // var result = ArrayStrings.MyAtoi("2147483657");
-           var result = ArrayStrings.LengthOfLongestSubstring("abcabcbb");
-            Console.WriteLine(result);
+            // var result = ArrayStrings.MyAtoi("2147483657");
+            // var result = ArrayStrings.LengthOfLongestSubstring("abcabcbb");
+            //var ts = new TwoSum();
+            //ts.Add(0);
+            //ts.Add(-1);
+            //ts.Add(-1);
+            //ts.Add(0);
+            //ArrayStrings.PlusOne(new int[] { 8, 9 });
+            //ArrayStrings.solution(new int[] { 1,1,2,3,3 }, 3);
+
+            //Console.WriteLine(ArrayStrings.solution(new int[] { 1, 1, 2, 3, 3 }, 4));
+            Console.WriteLine(TwoSum.ReturnAvgOfWindowSize(new int[] { 2,4,6,8}, 4));
             Console.ReadLine();
+        }
+    }
+
+    public class TwoSum
+    {
+
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        // Add the number to an internal data structure.
+        public void Add(int number)
+        {
+            if (!dict.ContainsKey(number)) dict[number] = 1;
+            else dict[number] = 2;
+        }
+
+        // Find if there exists any pair of numbers which sum is equal to the value.
+        public bool Find(int value)
+        {
+            foreach (int key in dict.Keys)
+            {
+                if (key == value - key)
+                {
+                    if (dict[key] == 2) return true;
+                }
+                else
+                    if (dict.ContainsKey(value - key)) return true;
+            }
+
+            return false;
+        }
+
+
+        public static ArrayList ReturnAvgOfWindowSize(int[] inputArr, int windowSize)
+        {
+            ArrayList outPut = new ArrayList();
+            for (int i = 0; i <= inputArr.Length-1; i++)
+            {
+                int sum = 0;
+                if(i+windowSize-1 >= inputArr.Length)
+                {
+                    return outPut;
+                }
+                for (int j = i; j <= i+ windowSize -1; j++)
+                {
+                    sum += inputArr[j];
+                }
+                outPut.Add(sum / windowSize);
+            }
+
+            return outPut;
         }
     }
 }
